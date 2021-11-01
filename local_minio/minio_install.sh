6b5dadd8050c40693ea92ae8910289df4efeb699
Jimmy Lafontaine <lafonj@gmail.com>
2021-11-01 11:55:27 -0400

    Initial files for local minio recipe

---
+++ local_minio/minio_install.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+
+IPS_ARR=(
+    192.168.89.6
+    192.168.89.13
+    192.168.89.20
+    192.168.89.5
+    192.168.89.17
+    192.168.89.10
+    192.168.88.253
+    192.168.89.21
+    192.168.89.0
+    192.168.89.22
+    192.168.89.4
+    192.168.89.8
+    192.168.89.14
+    192.168.89.9
+    192.168.89.2
+    192.168.88.254
+    192.168.89.1
+    192.168.89.24
+    192.168.89.29
+    192.168.89.103
+    192.168.88.252
+    192.168.89.16
+    192.168.89.11
+    192.168.89.18
+    192.168.89.7
+    192.168.89.3
+    192.168.89.19
+    192.168.89.15
+    192.168.89.64
+    192.168.89.65
+    192.168.89.104
+    192.168.89.91
+)
+ACCESS_KEY=access_key
+SECRET_KEY=secret_key
+GROUPID=jimmy-minio-server
+
+function do_install {
+  do_uninstall
+
+  for ((i=0;i<${#IPS_ARR[@]};++i)); do
+    echo "${IPS_ARR[i]} $GROUPID-$i" >> /etc/hosts
+  done
+
+  echo "Total: $i"
+  if [ ! -f /usr/bin/minio ]; then
+    curl -o /usr/bin/minio https://dl.min.io/server/minio/release/linux-arm64/minio
+    chmod +x /usr/bin/minio
+  fi
+  cp minio.service /lib/systemd/system/minio.service
+  chmod 664 /lib/systemd/system/minio.service
+  systemctl daemon-reload
+  service minio start
+}
+
+function do_uninstall {
+  rm -rfR /data/*
+  rm  -rfR /data/.*
+  sed -i "/$GROUPID/d" /etc/hosts
+  service minio stop
+  rm /lib/systemd/system/minio.service
+  systemctl daemon-reload
+}
+
+
+case "$1" in
+  install) do_install ;;
+  uninstall) do_uninstall ;;
+esac
---
+++ local_minio/service_lookup.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess
+import time
+
+
+def get_avahi_data():
+    try:
+        process = subprocess.Popen(
+            ['avahi-browse', '-r', '--domain=local', '_cachengo._tcp', '-p', '-k', '-c'],
+            stdout=subprocess.PIPE, 
+            stderr=subprocess.PIPE
+        )
+        stdout, _ = process.communicate()
+        return stdout
+    except:
+        return ''
+
+
+def parse_avahi_data(data, interface='eth0', ip_type='IPv4'):
+    lines = data.splitlines()
+    result = {}
+    for line_b in lines:
+        line = line_b.decode("utf-8")
+        if line[0] != '=':
+            continue
+        info = line.split(';')
+        if info[1] != interface or info[2] != ip_type:
+            continue
+        result[info[3]] = info[7]
+    return result
+
+
+def is_ip_up(ip):
+    response = os.system(f'ping -c 1 {ip}')
+    return response == 0
+
+
+def set_ip_for_host(hostname, ip):
+    filename = '/etc/hosts'
+    with open(filename, "r") as f:
+        lines = f.readlines()
+
+    with open(filename, "w+") as f:
+        for line in lines:
+            if hostname not in line:
+                f.write(line)
+        f.write(f'{ip} {hostname}\n')
+
+
+def restart_service():
+    process = subprocess.Popen(
+        ['service', 'minio', 'restart'],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
+    process.communicate()
+
+
+def fetch_parse_avahi():
+    return parse_avahi_data(get_avahi_data())
+
+
+if __name__ == "__main__":
+
+    hostnames = ['B020111-958092d', 'B020112-9a4ebf6', 'B020113-2c652b2', 'B020114-8c52d97', 'B020111-ddd4188', 'B020122-5b92ff2', 'B020123-461cda3', 'B020124-0689480']
+    host_ip = {host: None for host in hostnames}
+
+    while True:
+        change_detected = False
+        data_fetched = False
+
+        for i, hostname in enumerate(hostnames):
+            if host_ip[hostname] is None or not is_ip_up(host_ip[hostname]):
+                if not data_fetched:
+                    info = fetch_parse_avahi()
+                    data_fetched = True
+                tries = 0
+                is_up = False
+                while not is_up and tries <= 10:
+                    name = hostname + f"-{tries}" if tries > 0 else hostname
+                    new_ip = info.get(name)
+                    if new_ip is not None:
+                        is_up = is_ip_up(new_ip)
+                        print(f'Is up: {new_ip}')
+                    tries += 1
+                    print('Try finished')
+                if is_up:
+                    print('Will set ip')
+                    set_ip_for_host(f'minio-server-{i}', new_ip)
+                    host_ip[hostname] = new_ip
+                    change_detected = True
+
+        if change_detected:
+            print('Restarting service')
+            restart_service()
+        else:
+            print('No changes')
+
+        time.sleep(50)
