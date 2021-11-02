ea05557b5cbef8e4c10170c99e2e64132311c420
Omar Campos <megathodik@gmail.com>
2021-11-02 15:54:22 -0400

    Modifications to minio_install to pass hostnames to service lookup

--- local_minio/minio_install.sh
+++ local_minio/minio_install.sh
@@ -1,69 +1,58 @@
 #!/bin/bash
+source "utils/parameters.sh"
 
+# IPS_ARR=(
 
-IPS_ARR=(
-    192.168.89.6
-    192.168.89.13
-    192.168.89.20
-    192.168.89.5
-    192.168.89.17
-    192.168.89.10
-    192.168.88.253
-    192.168.89.21
-    192.168.89.0
-    192.168.89.22
-    192.168.89.4
-    192.168.89.8
-    192.168.89.14
-    192.168.89.9
-    192.168.89.2
-    192.168.88.254
-    192.168.89.1
-    192.168.89.24
-    192.168.89.29
-    192.168.89.103
-    192.168.88.252
-    192.168.89.16
-    192.168.89.11
-    192.168.89.18
-    192.168.89.7
-    192.168.89.3
-    192.168.89.19
-    192.168.89.15
-    192.168.89.64
-    192.168.89.65
-    192.168.89.104
-    192.168.89.91
-)
-ACCESS_KEY=access_key
-SECRET_KEY=secret_key
 GROUPID=minio-server
 
 function do_install {
-  do_uninstall
+  # do_uninstall
 
-  for ((i=0;i<${#IPS_ARR[@]};++i)); do
-    echo "${IPS_ARR[i]} $GROUPID-$i" >> /etc/hosts
-  done
+  # cachengo-cli updateInstallStatus $APPID "Installing"
+  echo "HOSTNAMES is:"
+  echo $HOSTNAMES
+  local HOSTS_ARR
+  array_from_json_list HOSTS_ARR "$HOSTNAMES"
+  
+  export MINIO_ACCESS_KEY=$ACCESS_KEY
+  export MINIO_SECRET_KEY=$SECRET_KEY
 
-  echo "Total: $i"
-  if [ ! -f /usr/bin/minio ]; then
-    curl -o /usr/bin/minio https://dl.min.io/server/minio/release/linux-arm64/minio
-    chmod +x /usr/bin/minio
-  fi
-  cp minio.service /lib/systemd/system/minio.service
-  chmod 664 /lib/systemd/system/minio.service
-  systemctl daemon-reload
-  service minio start
+  
+  apt install -y avahi-utils
+  apt install -y python3
+  #TODO: call the service_lookup script
+
+  echo "Hosts array is:"
+  echo $HOSTS_ARR
+
+  python3 local_minio/service_lookup.py 
+
+  # for ((i=0;i<${#IPS_ARR[@]};++i)); do 
+  #   echo "${IPS_ARR[i]} $GROUPID-$i"
+  #   echo "${IPS_ARR[i]} $GROUPID-$i" >> /etc/hosts
+  # done
+
+  # echo "Total: $i"
+  # if [ ! -f /usr/bin/minio ]; then
+  #   curl -o /usr/bin/minio https://dl.min.io/server/minio/release/linux-arm64/minio
+  #   chmod +x /usr/bin/minio
+  # fi
+  # cp minio.service /lib/systemd/system/minio.service
+  # chmod 664 /lib/systemd/system/minio.service
+  # systemctl daemon-reload
+  # service minio start
 }
 
 function do_uninstall {
-  rm -rfR /data/*
-  rm  -rfR /data/.*
-  sed -i "/$GROUPID/d" /etc/hosts
-  service minio stop
-  rm /lib/systemd/system/minio.service
-  systemctl daemon-reload
+  echo "Uninstalling"
+  cachengo-cli updateInstallStatus $APPID "Uinstalling"
+  cachengo-cli updateInstallStatus $APPID "Uninstalled"
+  # rm -rfR /data/*
+  # rm  -rfR /data/.*
+  # sed -i "/$GROUPID/d" /etc/hosts
+  # service minio stop
+  # rm /lib/systemd/system/minio.service
+  # systemctl daemon-reload
 }
 
 
