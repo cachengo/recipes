4ccbb9e44f6170b4fd3036c803edd2fd60f401f4
Omar Campos <megathodik@gmail.com>
2021-11-02 17:57:17 -0400

    Almost ready for testing

--- local_minio/minio_install.sh
+++ local_minio/minio_install.sh
@@ -17,23 +17,25 @@ function do_install {
 
   apt install -y avahi-utils
   apt install -y python3
-  
+  apt install -y curl
 
   #output hostnames to use with lookup service to a common file
   echo $HOSTNAMES > /usr/bin/minio_lookup_hostnames.json
     
   # install lookup service files  
-  cp minio_lookup.service /lib/systemd/system/minio_lookup.service
+  cp local_minio/service_lookup.py /usr/bin/service_lookup.py
+  chmod +x /usr/bin/service_lookup.py
+  cp local_minio/minio_lookup.service /lib/systemd/system/minio_lookup.service
   chmod 664 /lib/systemd/system/minio_lookup.service
   systemctl daemon-reload
   service minio_lookup start
 
   
   if [ ! -f /usr/bin/minio ]; then
-    curl -o /usr/bin/minio https://dl.min.io/server/minio/release/linux-x86/minio
+    curl -o /usr/bin/minio https://dl.min.io/server/minio/release/linux-amd64q/minio
     chmod +x /usr/bin/minio
   fi
-  cp minio.service /lib/systemd/system/minio.service
+  cp local_minio/minio.service /lib/systemd/system/minio.service
   chmod 664 /lib/systemd/system/minio.service
   systemctl daemon-reload
   service minio start
--- local_minio/service_lookup.py
+++ local_minio/service_lookup.py
@@ -65,8 +65,9 @@ def fetch_parse_avahi():
 if __name__ == "__main__":
 
     # hostnames = ['B020111-958092d', 'B020112-9a4ebf6', 'B020113-2c652b2', 'B020114-8c52d97', 'B020111-ddd4188', 'B020122-5b92ff2', 'B020123-461cda3', 'B020124-0689480']
-    hostnames_json = os.environ.get('HOSTNAMES')
-    hostnames = json.loads(hostnames_json)
+    hostnames_json = open("/usr/bin/minio_lookup_hostnames.json","r")
+    jsonString = hostnames_json.readline()
+    hostnames = json.loads(jsonString)
 
     host_ip = {host: None for host in hostnames}
 
