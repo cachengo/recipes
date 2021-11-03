b02781252d76f37f4fe2f8384a791b044a51be3a
Omar Campos <megathodik@gmail.com>
2021-11-03 16:23:30 -0400

    Fixed sed command

--- local_minio/minio_install.sh
+++ local_minio/minio_install.sh
@@ -44,7 +44,7 @@ function do_install {
     curl -o /usr/bin/minio "http://dl.min.io/server/minio/release/linux-$platform/minio"
     chmod +x /usr/bin/minio
   fi
-  sed -i "s/#host_number#/${#HOSTS_ARR[@]}" /etc/hosts
+  sed -i "s/#host_number#/${#HOSTS_ARR[@]}/" local_minio/minio.service
   cp local_minio/minio.service /lib/systemd/system/minio.service
   chmod 664 /lib/systemd/system/minio.service
   systemctl daemon-reload
