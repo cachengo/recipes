f780cd8ee4ac97d5ce1bfc90e476443eb110e359
Raul Negron <rntnegron@gmail.com>
2022-02-10 13:40:36 -0400

    Moved network functions to utils folder

--- mariadb/run.sh
+++ mariadb/run.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
 source "utils/cachengo.sh"
-
-function create_network {
-  docker network inspect "$1" >/dev/null 2>&1 || \
-  docker network create --driver bridge "$1"
-}
+source "utils/docker.sh"
 
 function do_install {
   set -e
@@ -16,31 +12,25 @@ function do_install {
     NET_FLAG="--network $NETWORK"
   fi
 
-  docker run \
-    -d \
-    --name "$APPID" \
-    -e MARIADB_USER="$DB_USER" \
-    -e MARIADB_PASSWORD="$DB_PASSWORD" \
-    -e MARIADB_DATABASE="$DATABASE_NAME" \
-    -e MARIADB_ROOT_PASSWORD="$ROOT_PASSWORD" \
-    -e MARIADB_ALLOW_EMPTY_ROOT_PASSWORD="$ALLOW_EMPTY_ROOT_PASSWORD" \
-    -e MARIADB_RANDOM_ROOT_PASSWORD="$RANDOM_ROOT_PASSWORD" \
-    -v "/data/$APPID":/var/lib/mysql \
-    -p "$HOST_PORT":3306 \
-    $NET_FLAG \
-    mariadb:10.7
-  cachengo-cli updateInstallStatus "$APPID" "Installed"
-}
-
-function delete_network {    
-  if [ $( docker network inspect "$1" | grep '"Containers": {},' | wc -l ) -gt 0 ]; then
-    docker network rm "$1"
-  fi  
-}
-
-function get_network { 
-  NETWORK_NAME=$(docker inspect "$APPID" | jq '.[0].NetworkSettings.Networks' | jq 'keys[0]' | cut -d '"' -f 2)
-  echo "$NETWORK_NAME"
+  if [[ $( docker --version ) ]]; then
+    docker run \
+      -d \
+      --name "$APPID" \
+      -e MARIADB_USER="$DB_USER" \
+      -e MARIADB_PASSWORD="$DB_PASSWORD" \
+      -e MARIADB_DATABASE="$DATABASE_NAME" \
+      -e MARIADB_ROOT_PASSWORD="$ROOT_PASSWORD" \
+      -e MARIADB_ALLOW_EMPTY_ROOT_PASSWORD="$ALLOW_EMPTY_ROOT_PASSWORD" \
+      -e MARIADB_RANDOM_ROOT_PASSWORD="$RANDOM_ROOT_PASSWORD" \
+      -v "/data/$APPID":/var/lib/mysql \
+      -p "$HOST_PORT":3306 \
+      $NET_FLAG \
+      mariadb:10.7
+    cachengo-cli updateInstallStatus "$APPID" "Installed"
+  else
+    echo "Docker is not installed"
+    exit 1 
+  fi
 }
 
 function do_uninstall {
