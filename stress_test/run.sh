4387826a8d88af68d04a940c1ab15b0d7bdc2e28
Brad Churchwell <wchurchwel@gmail.com>
2022-07-05 15:17:57 -0500

    moved dependencies to data dir

--- stress_test/run.sh
+++ stress_test/run.sh
@@ -2,50 +2,40 @@
 
 source "utils/cachengo.sh"
 
+DATADIR=/data/$APPID
+
 function do_install {
   set -e
   cachengo-cli updateInstallStatus $APPID "Installing"
+  cachengo-cli updateInstallStatus $APPID "Installing: Network Stress Test"
 
   if [ "$SERVER" == "true" ]; then
-    cachengo-cli updateInstallStatus $APPID "Installing: Network Stress Test"
+    NETWORKSERVICE=stress_network_server.service
     apt install iperf -y
     cp stress_test/stress_network_server.service /lib/systemd/system/
-    cachengo-cli updateInstallStatus $APPID "Installing: CPU Stress Test"
-    apt install stress -y
-    cp stress_test/stress_cpu.service /lib/systemd/system/
-    cachengo-cli updateInstallStatus $APPID "Installing: GPU Stress Test"
-    apt install hashcat -y
-    cp stress_test/stress_gpu.service /lib/systemd/system/
-    cd / && curl -L https://github.com/hashcat/hashcat/archive/refs/tags/v5.1.0.zip -o hashcat-5.1.0.zip && cd -
-   # cp stress_test/hashcat-5.1.0.7z /
-   # apt install p7zip -y
-   # p7zip -d /hashcat-5.1.0.7z
-    unzip /hashcat-5.1.0.zip -d /
-    cachengo-cli updateInstallStatus $APPID "Installing: Starting Stress Test"
-    systemctl enable stress_network_server.service stress_cpu.service stress_gpu.service
-    systemctl daemon-reload
-    systemctl start stress_network_server.service stress_cpu.service stress_gpu.service
   else
-    cachengo-cli updateInstallStatus $APPID "Installing: Network Stress Test"
+    NETWORKSERVICE=stress_network_client.service
     apt install iperf -y
     sed -i "s/#server_ip#/$SERVER_IP/" stress_test/stress_network_client.service
     cp stress_test/stress_network_client.service /lib/systemd/system/
-    cachengo-cli updateInstallStatus $APPID "Installing: CPU Stress Test"
-    apt install stress -y
-    cp stress_test/stress_cpu.service /lib/systemd/system/
-    cachengo-cli updateInstallStatus $APPID "Installing: GPU Stress Test"
-    apt install hashcat -y
-    cp stress_test/stress_gpu.service /lib/systemd/system/
-    cd / && curl -L https://github.com/hashcat/hashcat/archive/refs/tags/v5.1.0.zip -o hashcat-5.1.0.zip && cd -
-   # cp stress_test/hashcat-5.1.0.7z /
-   # apt install p7zip -y
-   # p7zip -d /hashcat-5.1.0.7z
-    unzip /hashcat-5.1.0.zip -d /
-    cachengo-cli updateInstallStatus $APPID "Installing: Starting Stress Test"
-    systemctl enable stress_network_client.service stress_cpu.service stress_gpu.service
-    systemctl daemon-reload
-    systemctl start stress_network_client.service stress_cpu.service stress_gpu.service
   fi
+
+  cachengo-cli updateInstallStatus $APPID "Installing: CPU Stress Test"
+  apt install stress -y
+  cp stress_test/stress_cpu.service /lib/systemd/system/
+
+  cachengo-cli updateInstallStatus $APPID "Installing: GPU Stress Test"
+  apt install hashcat -y
+  sed -i "s|#datadir#|$DATADIR|g" stress_test/stress_gpu.service
+  cp stress_test/stress_gpu.service /lib/systemd/system/
+  mkdir -p $DATADIR
+  cd $DATADIR && curl -L https://github.com/hashcat/hashcat/archive/refs/tags/v5.1.0.zip -o hashcat-5.1.0.zip && cd -
+  unzip $DATADIR/hashcat-5.1.0.zip -d $DATADIR
+
+  cachengo-cli updateInstallStatus $APPID "Installing: Starting Stress Test"
+  systemctl enable $NETWORKSERVICE stress_cpu.service stress_gpu.service
+  systemctl daemon-reload
+  systemctl start $NETWORKSERVICE stress_cpu.service stress_gpu.service
 
   cachengo-cli updateInstallStatus $APPID "Installed"
 }
@@ -56,6 +46,7 @@ function do_uninstall {
   rm -rf /lib/systemd/system/stress_network_*.service
   rm -rf /lib/systemd/system/stress_cpu.service
   rm -rf /lib/systemd/system/stress_gpu.service
+  rm -rf $DATADIR
   cachengo-cli updateInstallStatus $APPID "Uninstalled"
 }
 
