2dd1780819f0add8786067f992612f6741d0026d
trombonebuster <wchurchwel@gmail.com>
2022-08-31 20:11:10 -0500

    bring in federated minio

---
+++ federated_minio/minio_install.sh
@@ -0,0 +1,115 @@
+http://#group_id#-{0...#host_number#}/data/#group_id#
+
+#!/bin/bash
+source "utils/parameters.sh"
+
+function do_install {
+  uninstall_only
+
+  echo "Installation started"
+  cachengo-cli updateInstallStatus $APPID "Installing"
+  local HOSTS_ARR
+  array_from_json_list HOSTS_ARR "$HOSTNAMES"
+  array_len=$((${#HOSTS_ARR[@]}-1 ))
+  clusters_num=$CLUSTER_NUM
+  CLUSTERS=''
+  cluster_end=0
+  cluster_start=0
+
+  for ((i=0;i<$CLUSTER_NUM;++i)); do
+    cluster_end=$(($cluster_end+$CLUSTER_SIZE))
+    CLUSTERS+=('http://$GROUPID-{$cluster_start...$cluster_end}/data/$GROUPID ')
+    cluster_start=$(($cluster_start+$CLUSTER_SIZE))
+  done
+
+  export MINIO_ACCESS_KEY=$ACCESS_KEY
+  export MINIO_SECRET_KEY=$SECRET_KEY
+
+  apt install -y avahi-utils
+  apt install -y python3
+  apt install -y curl
+
+  echo "Installing hostname lookup service" 
+  sed -i "s/#hostnames_json#/$HOSTNAMES/" baremetal_minio/minio_lookup.service
+  sed -i "s/#group_id#/$GROUPID/" baremetal_minio/minio_lookup.service
+  sed -i "s/#hostnames_json#/$HOSTNAMES/" baremetal_minio/restart_avahi.service
+  sed -i "s/#group_id#/$GROUPID/" baremetal_minio/restart_avahi.service
+  cp baremetal_minio/service_lookup.py /usr/bin/service_lookup.py
+  cp baremetal_minio/restart_avahi.py /usr/bin/restart_avahi.py
+  chmod +x /usr/bin/service_lookup.py
+  chmod +x /usr/bin/restart_avahi.py
+  cp baremetal_minio/minio_lookup.service /lib/systemd/system/minio_lookup.service
+  cp baremetal_minio/minio_lookup.timer /lib/systemd/system/minio_lookup.timer
+  cp baremetal_minio/restart_avahi.service /lib/systemd/system/restart_avahi.service
+  cp baremetal_minio/restart_avahi.timer /lib/systemd/system/restart_avahi.timer
+  chmod 664 /lib/systemd/system/minio_lookup.service
+  chmod 664 /lib/systemd/system/restart_avahi.service
+  chmod 664 /lib/systemd/system/minio_lookup.timer
+  chmod 664 /lib/systemd/system/restart_avahi.timer
+  systemctl daemon-reload
+  systemctl enable restart_avahi.timer
+  systemctl enable minio_lookup.timer
+  systemctl start minio_lookup.service
+  systemctl start restart_avahi.service
+
+  platform=`uname -m`
+  if [[ $platform == x86_64 ]]; then
+    platform=amd64
+  fi
+
+  if [[ $platform == aarch64 ]]; then
+    platform=arm64
+  fi
+
+
+  if [ ! -f /usr/bin/minio ]; then
+    echo "Downloading Min.io"
+    curl -L -o /usr/bin/minio "http://dl.min.io/server/minio/release/linux-$platform/minio"
+    chmod +x /usr/bin/minio
+  fi
+  echo "Installing Min.io service"
+  sed -i "s/#access_key#/$ACCESS_KEY/" baremetal_minio/minio.service
+  sed -i "s/#secret_key#/$SECRET_KEY/" baremetal_minio/minio.service
+  sed -i "s/#clusters#/$CLUSTERS/" baremetal_minio/minio.service
+  sed -i "s/#group_id#/$GROUPID/g" baremetal_minio/minio.service
+  cp baremetal_minio/minio.service /lib/systemd/system/minio.service
+  chmod 664 /lib/systemd/system/minio.service
+  systemctl daemon-reload
+  service minio start
+  service avahi-daemon restart
+  echo "Installation Successful"
+}
+
+function uninstall_only {
+  echo "Stoping Services"
+  service minio stop
+  service minio_lookup stop
+
+  echo "Removing services files"
+  rm /lib/systemd/system/minio.service
+  rm /lib/systemd/system/minio_lookup.service
+  
+  echo "Removing data"
+  rm -rfR /data/$GROUPID/
+  
+  echo "Removing support files"
+  rm /usr/bin/service_lookup.py
+  rm /usr/bin/minio
+  systemctl daemon-reload
+  
+  echo "Cleaning host files"
+  sed -i "/$GROUPID/d" /etc/hosts
+  echo "Uninstallation Successful"
+}
+
+function do_uninstall {
+  cachengo-cli updateInstallStatus $APPID "Uninstalling"
+  uninstall_only
+  cachengo-cli updateInstallStatus $APPID "Uninstalled"
+}
+
+
+case "$1" in
+  install) do_install ;;
+  uninstall) do_uninstall ;;
+esac
