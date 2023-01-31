74fb422eb038daab72e8444ed781e66f05b215af
Raul Negron <rntnegron@gmail.com>
2023-01-31 18:42:33 -0400

    added appid as namespace

--- grafana/run.sh
+++ grafana/run.sh
@@ -1,43 +1,9 @@
 #!/bin/bash
 
-<<<<<<< Updated upstream
-source "utils/cachengo.sh"
-source "grafana/grafana-configmap.yaml"
-source "grafana/grafana-deployment.yaml"
-source "grafana/grafana-ingress.yaml"
-source "grafana/grafana-pvc.yaml"
-source "grafana/grafana-service.yaml"
-
-
-=======
->>>>>>> Stashed changes
 function do_install {
   set -e
   cachengo-cli updateInstallStatus "$APPID" "Installing"
 
-<<<<<<< Updated upstream
-  if [ -n "$NAMESPACE_NAME" ]; then
-
-  kubectl create namespace $NAMESPACE_NAME
-  sed -i "s|#namespace_name#|$NAMESPACE_NAME|g" grafana/grafana-configmap.yaml
-  sed -i "s|#namespace_name#|$NAMESPACE_NAME|g" grafana/grafana-deployment.yaml
-  sed -i "s|#namespace_name#|$NAMESPACE_NAME|g" grafana/grafana-ingress.yaml
-  sed -i "s|#namespace_name#|$NAMESPACE_NAME|g" grafana/grafana-service.yaml
-  sed -i "s|#namespace_name#|$NAMESPACE_NAME|g" grafana/grafana-pvc.yaml
-
-
-  else
-
-  sed -i "s|#namespace_name#|default|g" grafana/grafana-configmap.yaml
-  sed -i "s|#namespace_name#|default|g" grafana/grafana-deployment.yaml
-  sed -i "s|#namespace_name#|default|g" grafana/grafana-ingress.yaml
-  sed -i "s|#namespace_name#|default|g" grafana/grafana-service.yaml
-  sed -i "s|#namespace_name#|default|g" grafana/grafana-pvc.yaml
-
-
-  fi
-
-=======
   kubectl create namespace $APPID
 
   sed -i "s|#namespace_name#|$APPID|g" grafana/grafana-configmap.yaml
@@ -45,7 +11,6 @@ function do_install {
   sed -i "s|#namespace_name#|$APPID|g" grafana/grafana-ingress.yaml
   sed -i "s|#namespace_name#|$APPID|g" grafana/grafana-service.yaml
   sed -i "s|#namespace_name#|$APPID|g" grafana/grafana-pvc.yaml
->>>>>>> Stashed changes
 
   kubectl apply -f grafana/grafana-configmap.yaml
   kubectl apply -f grafana/grafana-deployment.yaml
@@ -58,13 +23,6 @@ function do_install {
 function do_uninstall {
   cachengo-cli updateInstallStatus "$APPID" "Uninstalling"
   
-<<<<<<< Updated upstream
-  kubectl delete -f grafana/grafana-configmap.yaml
-  kubectl delete -f grafana/grafana-deployment.yaml
-  kubectl delete -f grafana/grafana-ingress.yaml
-  kubectl delete -f grafana/grafana-service.yaml
-  kubectl delete -f grafana/grafana-pvc.yaml
-=======
   kubectl delete -f grafana/grafana-ingress.yaml -n $APPID
   kubectl delete -f grafana/grafana-service.yaml -n $APPID
   kubectl delete -f grafana/grafana-deployment.yaml -n $APPID
@@ -72,7 +30,6 @@ function do_uninstall {
   kubectl delete -f grafana/grafana-configmap.yaml -n $APPID
 
   kubectl delete namespace $APPID
->>>>>>> Stashed changes
 
   cachengo-cli updateInstallStatus "$APPID" "Uninstalled"
 }
