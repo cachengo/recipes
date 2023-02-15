d817a69509d91ce8917ba16f5b9e12f261f08c3e
Omar <megathodik@gmail.com>
2023-02-15 16:54:25 -0400

    Changed erroneous permissions on files

--- argos_k3s/argos_install.sh
+++ argos_k3s/argos_install.sh
@@ -5,34 +5,34 @@ function do_install {
     # zookeeper
     echo "Installing Zookeeper into k3s cluster"
     kubectl create ns zookeeper
-    kubectl apply -f zookeeper/zookeeper.yaml
+    kubectl apply -f argos_k3s/zookeeper/zookeeper.yaml
 
     # nginx
     echo "Creating nginx ingress"
-    ./nginx/create-nginx.sh
+    ./argos_k3s/nginx/create-nginx.sh
 
     # minio
     echo "Installing minio"
-    kubectl apply -f minio/minio-namespace.yaml -f minio/minio-service.yaml -f minio/minio-pvc.yaml -f minio/minio-ingress.yaml -f minio/minio-deployment.yaml 
+    kubectl apply -f argos_k3s/minio/minio-namespace.yaml -f argos_k3s/minio/minio-service.yaml -f argos_k3s/minio/minio-pvc.yaml -f argos_k3s/minio/minio-ingress.yaml -f argos_k3s/minio/minio-deployment.yaml 
 
     # clickhouse
-    ./clickhouse/create-clickhouse.sh 
-``
+    ./argos_k3s/clickhouse/create-clickhouse.sh 
+
     sleep 60
 
     kubectl -n clickhouse exec chi-pv-log-deployment-pv-0-0-0 -- clickhouse-client --query "create database argos on cluster 'deployment-pv';"
 
     #argos
-    kubectl apply -f argos-namespace.yaml -f argos-ingress.yaml -f argos-pvc.yaml -f argos-role.yaml -f argos-service.yaml -f argos-deployment.yaml
+    kubectl apply -f argos_k3s/argos/argos-namespace.yaml -f argos_k3s/argos/argos-ingress.yaml -f argos_k3s/argos/argos-pvc.yaml -f argos_k3s/argos/argos-role.yaml -f argos_k3s/argos/argos-service.yaml -f argos_k3s/argos/argos-deployment.yaml
     cachengo-cli updateInstallStatus $APPID "Installed"
 }
 
 function do_uninstall {
     cachengo-cli updateInstallStatus $APPID "Uninstalling"
-    kubectl delete -f zookeeper/zookeeper.yaml 
-    kubectl delete -f minio/minio/minio-deployment.yaml -f minio/minio-service.yaml -f minio/minio-pvc.yaml -f minio/minio-ingress.yaml -f minio/minio-namespace.yaml
-    kubectl delete -f clickhouse/clickhouse-deployment.yaml
-    kubectl delete -f argos/argos-deployment.yaml -f argos-ingress.yaml -f argos-pvc.yaml -f argos-role.yaml -f argos-service.yaml -f argos/argos-namespace.yaml    
+    kubectl delete -f argos_k3s/zookeeper/zookeeper.yaml 
+    kubectl delete -f argos_k3s/minio/minio-deployment.yaml -f argos_k3s/minio/minio-service.yaml -f argos_k3s/minio/minio-pvc.yaml -f argos_k3s/minio/minio-ingress.yaml -f argos_k3s/minio/minio-namespace.yaml
+    kubectl delete -f argos_k3s/clickhouse/clickhouse-deployment.yaml
+    kubectl delete -f argos_k3s/argos/argos-deployment.yaml -f argos_k3s/argos/argos-ingress.yaml -f argos_k3s/argos/argos-pvc.yaml -f argos_k3s/argos/argos-role.yaml -f argos_k3s/argos/argos-service.yaml -f argos_k3s/argos/argos-namespace.yaml    
     cachengo-cli updateInstallStatus $APPID "Uninstalled"
 }
 
