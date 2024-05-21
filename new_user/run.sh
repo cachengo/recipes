dcd32a140f9febdd6b1c13d448969249fbd89fc6
trombonebuster <wchurchwel@gmail.com>
2024-05-21 11:47:47 -0500

    fix create user

--- new_user/run.sh
+++ new_user/run.sh
@@ -5,14 +5,31 @@ source "utils/cachengo.sh"
 function do_install {
   set -e
   cachengo-cli updateInstallStatus $APPID "Installing"
-  # Remove user@host to add our own identifier
-  useradd -s /path/to/shell -d /home/{dirname} -m -G sudo
+
+  adduser --gecos "$USERNAME" \
+          --disabled-password \
+          --shell /bin/bash \
+    --home /home/$USERNAME \
+          "$USERNAME"
+
+  #adduser --home /home/.cachengo cachengo 
+  usermod -aG sudo $USERNAME
+  #chown -R cachengo:cachengo /home/.cachengo
+  mkdir /home/$USERNAME/.ssh
+  touch /home/$USERNAME/.ssh/authorized_keys
+  chmod 700 /home/$USERNAME/.ssh
+  chmod 600 /home/$USERNAME/.ssh/authorized_keys
+  chown -R $USERNAME:$USERNAME /home/$USERNAME
+  #usermod -aG docker cachengo
+  echo "$USERNAME:$USERNAME" | chpasswd
+
   cachengo-cli updateInstallStatus $APPID "Installed"
 }
 
 function do_uninstall {
   cachengo-cli updateInstallStatus $APPID "Uninstalling"
-  sed -i "/$APPID/d" /home/*/.ssh/authorized_keys
+  deluser --remove-home $USERNAME
+  # sed -i "/$APPID/d" /home/*/.ssh/authorized_keys
   cachengo-cli updateInstallStatus $APPID "Uninstalled"
 }
 
